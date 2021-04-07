ddfdd5fdf0bba71e3b0689758d783917ae3b3af8
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-04-07 15:06:59 -0700

    Add getPlayerSelection function to prompt user for choice

--- js/app.js
+++ js/app.js
@@ -11,6 +11,17 @@ function createComputerPlay() {
   computerSelection = choices[createRandomNum(3)];
 }
 
+function getPlayerSelection() {
+  playerSelection = '';
+
+  while (!(
+    playerSelection === choices[0] ||
+    playerSelection === choices[1] ||
+    playerSelection === choices[2])) {
+    playerSelection = prompt('Choose rock, paper or scissors!').toLowerCase();
+  }
+}
+
 function playRound() {
   createComputerPlay();
 }
