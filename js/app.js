4ca993ad561d0836f634966b3c95362e480bddba
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-04-07 14:57:30 -0700

    Add playerSelection variable

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const choices = ['rock', 'paper', 'scissors'];
 
 let computerSelection = '';
+let playerSelection = '';
 
 function createRandomNum(max) {
   return Math.floor(Math.random() * max);
