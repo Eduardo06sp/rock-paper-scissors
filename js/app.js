fdb14851f2c7dbb77690d52c1d4d2574a9be2083
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-04-07 14:49:17 -0700

    Add createComputerPlay() to enable computer to make random selection

--- js/app.js
+++ js/app.js
@@ -3,3 +3,7 @@ const choices = ['rock', 'paper', 'scissors'];
 function createRandomNum(max) {
   return Math.floor(Math.random() * max);
 }
+
+function createComputerPlay() {
+  computerSelection = possibilities[createRandomNum(3)];
+}
